d32bb14a114ae00c96b36b432d8330cb2b09b085
Miguel Hernandez <jhernandcab@gmail.com>
2019-04-19 18:02:49 -0500

    Cloning data from ine's course-starter-r

---
+++ exercises/test_01_03.R
@@ -0,0 +1,6 @@
+test <- function() {
+    # Here we can either check objects created in the solution code.
+    if (some_var != length(mtcars)) {
+        stop("Are you getting the correct length?")
+    }
+}
