255b03d99d3ca6777068f04b17bfc38042073440
Miguel Hernandez <jhernandcab@gmail.com>
2019-04-20 22:41:49 -0500

    recursion index

--- exercises/test_01_03.R
+++ exercises/test_01_03.R
@@ -1,6 +1,8 @@
 test <- function() {
-    # Here we can either check objects created in the solution code.
-    if (some_var != length(mtcars)) {
-        stop("Are you getting the correct length?")
-    }
+  # Here we can either check objects created in the solution code.
+  if (some_var != length(mtcars)) {
+    stop("Are you getting the correct length?")
+  } else {
+    print("Good job!")
+  }
 }
